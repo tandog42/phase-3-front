4c051ab00470e823800661aacce8eba337199238
Tanner <troxs690@gmail.com>
2022-11-02 09:11:41 -0500

    download MUI/created some comps

---
+++ src/EmployeBTN.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function EmployeBTN() {
+  return (
+    <div>EmployeBTN</div>
+  )
+}
+
+export default EmployeBTN
---
+++ src/EmployeeCont.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Employee from './Employee'
+function EmployeeCont() {
+  return (
+    <div>EmployeeCont</div>
+  )
+}
+
+export default EmployeeCont
