8be9c2f3d4089f6f34f14dc31cb92ba9d3116120
Tanner <troxs690@gmail.com>
2022-11-29 17:34:30 -0600

    added css to company comp

--- src/Company.js
+++ src/Company.js
@@ -1,11 +1,15 @@
 import React from "react";
+
 import { useParams } from "react-router-dom";
 
 function Company({ companies, setCompanies }) {
   let { id } = useParams();
 
   let comp = companies.find(com => com.id == id);
-  return <h1 id="companyTitle">{comp.company_name}</h1>;
+  return <div>
+    <h1 id="companyHeader">{comp.company_name}</h1>
+    </div>
+
 }
 //useparams
 //find
