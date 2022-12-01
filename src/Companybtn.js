c95aaaf4a36b1bcc1197acb6d83abf50f93d6e28
Tanner <troxs690@gmail.com>
2022-12-01 14:57:28 -0600

    trying to filter employees work in progress

--- src/Companybtn.js
+++ src/Companybtn.js
@@ -4,14 +4,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Companybtn({ companies }) {
+
+    const cc = companies.map(comp => {
+      comp.employees.filter(c => console.log(c.company_id))
+    })
+   
+   
+ 
+
   return (
     <div id="company">
       {companies.map(company => (
-        <Link key={company.id} to={'company/' + company.id }>
-        <Button id="comBTN" key={company.city}>
-          {company.company_name}
-        </Button>
-          </Link>
+        <Link key={company.id} to={"company/" + company.id}>
+          <Button id="comBTN" key={company.city}>
+            {company.company_name}
+          </Button>
+        </Link>
       ))}
     </div>
   );
