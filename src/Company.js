77907340561a189fff57c449b92eda7076397141
Tanner <troxs690@gmail.com>
2022-12-01 14:57:10 -0600

    cleaning

--- src/Company.js
+++ src/Company.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 import { useParams } from "react-router-dom";
+import Employees from "./Employees";
 
 function Company({ companies, setCompanies }) {
   let { id } = useParams();
 
-  let comp = companies.find(com => com.id == id);
+  let comp = companies.find(com => com.id === id);
   return <div>
     <h1 id="companyHeader">{comp.company_name}</h1>
+    <Employees companies={companies} setCompanies={setCompanies}/>
     </div>
+    
 
 }
 //useparams
