18f9442a3c763e0a36d2376b8a238c11cf3c641b
Tanner <troxs690@gmail.com>
2022-12-23 13:20:53 -0600

    fixed error

--- src/Company.js
+++ src/Company.js
@@ -6,14 +6,20 @@ import Employees from "./Employees";
 function Company({ companies, setCompanies }) {
   let { id } = useParams();
 
-  let comp = companies.find(com => com.id === id);
-  return <div>
-    <h1 id="companyHeader">{comp.company_name}</h1>
-    <Employees companies={companies} setCompanies={setCompanies}/>
-    </div>
-    
+  let comp = companies.find(com => com.id == id);
 
+  return comp ? (
+    <div>
+      <h1 id="companyHeader">{comp.company_name}</h1>
+      <Employees
+        comp={comp}
+        companies={companies}
+        setCompanies={setCompanies}
+      />
+    </div>
+  ) : (
+    <div></div>
+  );
 }
-//useparams
-//find
+
 export default Company;
