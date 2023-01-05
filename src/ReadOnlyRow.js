d9d396b2eb4277f4a693b90e56974b5fafa756d3
Tanner <troxs690@gmail.com>
2023-01-05 16:35:38 -0600

    created Delete fetch

--- src/ReadOnlyRow.js
+++ src/ReadOnlyRow.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React from "react";
 
-function ReadOnlyRow({emp, handleEditClick}) {
-
-  
+function ReadOnlyRow({ employees, setEmployees, emp, handleEditClick }) {
+  function onDeleteEmployee() {
+    const deletedEmployee = employees.filter(
+      employee => employee.id !== emp.id
+    );
+    setEmployees(deletedEmployee);
+    fetch(`http://localhost:9292/employees/${emp.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "apllication/json",
+      },
+    });
+  }
   return (
     <tr key={emp.id}>
-    <td>{emp.first_name}</td>
-    <td>{emp.last_name}</td>
-    <td>{emp.position}</td>
-    <td>${emp.salary}</td>
-    <td>
-      <button onClick={e => handleEditClick(e, emp)}>Edit</button>
-    </td>
-  </tr>
-  )
+      <td>{emp.first_name}</td>
+      <td>{emp.last_name}</td>
+      <td>{emp.position}</td>
+      <td>${emp.salary}</td>
+      <td>
+        <button onClick={e => handleEditClick(e, emp)}>Edit</button>
+        <button onClick={e => onDeleteEmployee()}>Delete</button>
+      </td>
+    </tr>
+  );
 }
 
-export default ReadOnlyRow
+export default ReadOnlyRow;
